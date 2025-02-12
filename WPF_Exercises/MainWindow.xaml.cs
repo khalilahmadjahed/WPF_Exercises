8af538b2ead35f4c3c5ebeb42c040b5226352fff
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-02-12 15:53:08 +0430

    Grid is learned!

--- WPF_Exercises/MainWindow.xaml.cs
+++ WPF_Exercises/MainWindow.xaml.cs
@@ -29,5 +29,50 @@ namespace WPF_Exercises
         {
             MessageBox.Show("Hamid tartug");
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Black!");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Red!");
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Green!");
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is White!");
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Yellow!");
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Blue!");
+        }
+
+        private void btn_7_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Orange!");
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Pure!");
+        }
+
+        private void Button_Click_8(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This color is Magenta!");
+        }
     }
 }
