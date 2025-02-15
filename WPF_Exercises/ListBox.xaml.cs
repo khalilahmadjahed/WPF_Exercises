fe2af9e9924a9e14bfd9c85262f3c912fb6c0cbc
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-02-15 20:48:14 +0430

    The first match is added Using user constructor

--- WPF_Exercises/ListBox.xaml.cs
+++ WPF_Exercises/ListBox.xaml.cs
@@ -23,7 +23,8 @@ namespace WPF_Exercises
         {
             InitializeComponent();
             List<Match> matches = new List<Match>();
-            matches.Add(new Match() { Team1 = "Barcellona", Score1 = 4, Score2 = 0, Team2 = "RealMadrid", Comprition = 85 }); 
+            //matches.Add(new Match() { Team1 = "Barcellona", Score1 = 4, Score2 = 0, Team2 = "RealMadrid", Comprition = 85 }); 
+            matches.Add(new Match("Barca", 4, 0, "Real", 85));
             lbMatches.ItemsSource = matches;
         }
     }
@@ -34,6 +35,13 @@ namespace WPF_Exercises
         public int Score2 { get; set; }
         public string Team2 { get; set; }
         public int Comprition { get; set; }
-       
+        public Match(string team1, int score1, int score2, string team2, int comprition)
+        {
+            Team1 = team1;
+            Score1 = score1;
+            Score2 = score2;
+            Team2 = team2;
+            Comprition = comprition;
+        }
     }
 }
