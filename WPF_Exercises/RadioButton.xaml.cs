1e62780ebc23c10a5fb2603b2cf6ff7b792f3360
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-02-17 15:41:17 +0430

    RadioButton is done!

---
+++ WPF_Exercises/RadioButton.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Exercises
+{
+    /// <summary>
+    /// Interaction logic for RadioButton.xaml
+    /// </summary>
+    public partial class RadioButton : Window
+    {
+        public RadioButton()
+        {
+            InitializeComponent();
+        }
+        private void yes_chechked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Yes, we love you!");
+            
+        }
+        
+        private void no_checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("No, we hate you!");
+        }
+        private void maybe_checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Umm, 50 50");
+        }
+    }
+}
