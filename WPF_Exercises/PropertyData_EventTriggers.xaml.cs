0dbbbbcc918133ba70e6e325a06b0a750eea18ab
Khalil Ahmad Jahed <khalilahmadjahed2017@gmail.com>
2025-02-17 17:12:34 +0430

    PropertyData and EventTriggers are done!

---
+++ WPF_Exercises/PropertyData_EventTriggers.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Exercises
+{
+    /// <summary>
+    /// Interaction logic for PropertyData_EventTriggers.xaml
+    /// </summary>
+    public partial class PropertyData_EventTriggers : Window
+    {
+        public PropertyData_EventTriggers()
+        {
+            InitializeComponent();
+        }
+    }
+}
